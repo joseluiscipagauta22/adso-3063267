3ceef5a2f88d4eab4c49972dfea0f7883590ca5c
JoseCipagauta <jose.arbelaez2229@gmail.com>
2026-04-13 14:51:24 -0500

    validar jwt para persistencia

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LoginDto } from '../dtos/login.dto';
 import { AuthService } from '../services/auth.service';
+import { JwtAuthGuard } from '../guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +15,12 @@ export class AuthController {
             body.password,
         );
         return this.authService.login(user);
+    }
+
+    @Get('check-status')
+    @UseGuards(JwtAuthGuard) // Usa el guard de JWT que ya configuraste
+    checkStatus(@Request() req) {
+    // req.user viene del Payload del JWT
+    return this.authService.checkStatus(req.user); 
     }
 }
